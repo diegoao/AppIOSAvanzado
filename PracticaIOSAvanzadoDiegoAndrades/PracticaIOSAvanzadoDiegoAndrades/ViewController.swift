8b7dd6ff0268d8a0be7d71b32f6df1ead039177e
Diego Andrades <die_94_go@hotmail.com>
2024-02-23 12:55:13 +0100

    Empiezo a crear la práctica de IOS avanzado

---
+++ PracticaIOSAvanzadoDiegoAndrades/PracticaIOSAvanzadoDiegoAndrades/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticaIOSAvanzadoDiegoAndrades
+//
+//  Created by Macbook Pro on 23/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
